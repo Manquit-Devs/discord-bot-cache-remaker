071aea43a2c496fad4b9d3392aa006555cd9f291
guilhermeg2k <guilhermen2k@gmail.com>
2022-05-01 05:02:39 -0300

    Treating when fails to get youtube data

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 import json
 from sys import argv
 from shutil import copyfile
@@ -20,13 +21,18 @@ def fetch_song_info(song) -> Song:
 
     return song
 
+
 def remake_cache(file_path: str):
     with open(file_path, 'r') as cache_json_file:
         song_caches = json.load(cache_json_file)
         for song in song_caches:
-            for _property in song_caches[song]:
-                if song_caches[song][_property] is None:
-                    song_caches[song] = fetch_song_info(song_caches[song])
+            try:
+                for _property in song_caches[song]:
+                    if song_caches[song][_property] is None:
+                        song_caches[song] = fetch_song_info(song_caches[song])
+                        break
+            except:
+                continue
         song_json_object = json.dumps(song_caches)
     with open(file_path, 'w') as cache_json_file:
         cache_json_file.write(song_json_object)
@@ -35,16 +41,12 @@ def remake_cache(file_path: str):
 if __name__ == "__main__":
     number_of_arguments = len(argv)
     if number_of_arguments == 2:
-        file_path = argv[1]
-        create_backup(file_path)
-        remake_cache(file_path)
-        print('Cache successfully remade')
-        # try:
-        #     file_path = argv[1]
-        #     create_backup(file_path)
-        #     remake_cache(file_path)
-        #     print('Cache successfully remade')
-        # except:
-        #     print('Error to remake cache')
+        try:
+            file_path = argv[1]
+            create_backup(file_path)
+            remake_cache(file_path)
+            print('Cache successfully remade')
+        except:
+            print('Error to remake cache')
     else:
         print('Invalid number of arguments')
